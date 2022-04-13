8a81f782cb4a58973b67f726c8f3bd2c34ab3aae
benzilsoman <97996097+benzilsoman@users.noreply.github.com>
2022-04-13 00:30:27 -0400

    lab 3 done

---
+++ powershell/lab3.ps1
@@ -0,0 +1,3 @@
+﻿Get-Ciminstance -Class Win32_NetworkAdapterConfiguration |
+where { $_.ipenabled -eq "true"} |
+Format-Table -a description, index, ipaddress, ipsubnet, dnsdomain, dnsserversearchOrder
